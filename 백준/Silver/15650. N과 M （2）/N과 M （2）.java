ca9d5682564c77276c1af822cc0c913848bf0f90
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-02-27 16:43:26 +0900

    [Silver III] Title: N과 M (2), Time: 160 ms, Memory: 15952 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,46 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int M;
-	static int N;
-	static int[] numbers;
-	static int[] visited;
-	
+	private static int N, M;
+	private static int[] store;
+	private static int[] visit;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		numbers = new int[M];
-		visited = new int[N+1];
-		
-		selectNum(1,0);
-	}
-	
-	public static void selectNum(int index,int count) {
-		if(count==M) {
-			for(int i=0;i<M;i++) {
-				System.out.print(numbers[i]+" ");
+
+		store = new int[M];
+		visit = new int[N];
+
+		NM(0, 0, 0);
+
+	} // end main method
+
+	private static void NM(int t, int n, int m) {
+		if (m == M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(store[i] + " ");
 			}
 			System.out.println();
 			return;
 		}
-		
-		for(int i=index ;i<=N;i++) {
-			if(visited[i] == 0) {
-				numbers[count] = i;
-				visited[i] = 1;
-				selectNum(i,count+1);
-				visited[i] = 0;
+
+		for (int i = t; i < N; i++) {
+			if (visit[i] == 0) {
+				visit[i] = 1;
+				store[m] = i + 1;
+				NM(++t, n + 1, m + 1);
+				visit[i] = 0;
 			}
 		}
-	}
+
+	} // end NM()
+
 }
