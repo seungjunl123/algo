1f9dc92c3e73a856f4c8cb95ac232f8ee97947fc
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-04-13 20:08:57 +0900

    [Silver I] Title: 곱셈, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -8,9 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
-
 class Main {
-	static int A,B,C;
+	static long A, B, C;
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,14 +17,15 @@ class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		st = new StringTokenizer(br.readLine());
-		A = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		bw.write(mod(B)+"");
+		A = Long.parseLong(st.nextToken());
+		B = Long.parseLong(st.nextToken());
+		C = Long.parseLong(st.nextToken());
+
+		bw.write(mod(B) + "");
 		bw.flush();
 		bw.close();
 	}
+
 	// 시간 초과...
 //	private static int mod(int A, int B, int C) {
 //		if(B==1) return A;
@@ -35,17 +35,16 @@ class Main {
 //			return mod(A,B/2,C)*mod(A,B/2,C)*A%C;
 //		}
 //	}
-
-	private static long mod(int num) {
-		int halfB = num/2;
-		if(num==1) {
+	private static long mod(long num) {
+		if (num == 1) {
 			return A%C;
 		}
-		long tmp = mod(halfB);
-		if(num%2==1) {
-			return ((tmp*tmp)%C*(A%C))%C;
+		long tmp = mod(num/2);
+		if (num % 2 == 0) {
+			return (tmp * tmp%C) % C;
 		} else {
-			return (tmp*tmp)%C;
+			return ((tmp*tmp%C)*(A%C)) % C;
 		}
 	}
+
 }
