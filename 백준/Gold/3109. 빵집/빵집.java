f5c966f4730e887518b7d9c1b6321febada44724
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-04-24 15:37:21 +0900

    [Gold II] Title: 빵집, Time: 408 ms, Memory: 29836 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -12,6 +12,7 @@ class Main {
 	static boolean[][] map;
 	static final int[][] dir = {{-1,1},{0,1},{1,1}};
 	static int ans;
+    // DFS를 멈추기 위한 boolean 값
 	static boolean flag;
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -57,6 +58,10 @@ class Main {
 			if(!flag&&nr>=0&&nc>=0&&nr<R&&nc<C&&!map[nr][nc]) {
 				map[nr][nc]=true;
 				findRoad(nr,nc);
+                // visited 처리를 하면 다음 행의 DFS가 실행될 때
+                // 이전 행에서 지나갔던 항목을 다시 지나게 된다
+                // 어차피 이전 행에서 지나갔음에도 이어지지 않은 지점은
+                // 다음 행에서 그 곳을 지나가도 이어지지 않는다
 			}
 		}
 	}
