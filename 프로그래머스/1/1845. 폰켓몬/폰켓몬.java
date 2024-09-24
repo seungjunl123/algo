0ee4a6f12f09d01d5767b34cb8e039dc98578887
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-09-24 21:57:32 +0900

    [level 1] Title: 폰켓몬, Time: 6.09 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        
+        if(set.size() < nums.length/2){
+            answer = set.size();        
+        } else {
+            answer = nums.length/2;
+        }
+        
+        return answer;
+    }
+}
