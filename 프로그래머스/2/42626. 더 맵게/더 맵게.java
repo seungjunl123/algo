7a9d4e3d6dba1e559e23274018a9440afb2deb0d
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-09-24 22:42:07 +0900

    [level 2] Title: 더 맵게, Time: 1526.42 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
+        int size = scoville.length;
+        for(int scv : scoville){
+            pq.add(scv);
+        }
+        
+        while( size >1 && pq.peek() < K ){
+            int smallOne = pq.poll();
+            int smallTwo = pq.poll();
+            pq.add(smallOne+smallTwo*2);
+            
+            answer++;
+            size--;
+        }
+        
+        if(pq.size()==1 && pq.peek()<K){
+            answer = -1;
+        }
+        
+        
+        return answer;
+    }
+}
