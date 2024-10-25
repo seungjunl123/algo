3f238628988f88f8c450473e027a6671b66a6e84
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-10-25 12:33:41 +0900

    [level 2] Title: H-Index, Time: 1.77 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations);  // 인용 횟수를 오름차순으로 정렬
+        int answer = 0;
+        Arrays.sort(citations);
+        // System.out.println(citations[0]);
         
-        int n = citations.length;
-        for (int i = 0; i < n; i++) {
-            int h = n - i;  // h는 남은 논문의 수 (h번 이상 인용된 논문의 수)
-            if (citations[i] >= h) {  // 현재 논문의 인용 횟수가 h번 이상이면 H-Index 성립
-                return h;  // 이 시점에서 H-Index는 h
+        for(int i=0;i<citations.length;i++){
+            // System.out.println(citations[i]);
+            if(citations.length-i<=citations[i] ) {
+                return citations.length-i;
             }
         }
-        return 0;  // H-Index가 없으면 0 반환
+        return answer;
     }
 }
