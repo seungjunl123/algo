0dbda610238c67b8a672b6748e8d9d9dc81e5b9f
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-12-10 12:30:28 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 236 ms, Memory: 26448 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -2,68 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int T, N;
-    static int[] parents;
-    static List<Integer> list;
-    static int answer;
+	static int T,N;
+	static int[] parents;
+	static List<Integer> list;
+	static int answer;
+	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		for(int i=0;i<T;i++) {
+			// parents의 초기값은 자기 자신을 부모로
+			// 이후에 주어지는 루트에 따라 부모를 찾는다.
+			// 모든 탐색이 끝난 뒤 자기 자신이 부모인 놈이 루트
+			// 이후 조상 노드를 찾는 두 노드에서 각자 List로 조상을 저장하고
+			// 저장된 노드에서 자기 자신까지 역순으로 탐색하면서 두 노드의 조상이 달라지는 부분을 찾는다
+			N = Integer.parseInt(br.readLine());
+			parents = new int[N+1];
+			answer = 0;
+			
+			for(int j=0;j<N-1;j++) {
+				st = new StringTokenizer(br.readLine());
+				int parentNode = Integer.parseInt(st.nextToken());
+				int childNode = Integer.parseInt(st.nextToken());
+				
+				parents[childNode] = parentNode;
+//				System.out.println(childNode+"의 부모는 "+parentNode);
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			int nodeA = Integer.parseInt(st.nextToken());
+			int nodeB = Integer.parseInt(st.nextToken());
+			
+			// A 넣고
+			list = new ArrayList<>();
+			list.add(nodeA);
+			findRoutes(nodeA);
+			List<Integer> parentsA = list;
+			
+			// B 넣고
+			list = new ArrayList<>();
+			list.add(nodeB);
+			findRoutes(nodeB);
+			List<Integer> parentsB = list;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+//			System.out.println(parentsA);
+//			System.out.println(parentsB);
+			
+			answer = 0;
+			for(int j= 0;j< Math.min(parentsA.size(), parentsB.size());j++) {
+				int aAncestor = parentsA.get(parentsA.size()-1-j);
+				int bAncestor = parentsB.get(parentsB.size()-1-j);
+				if(aAncestor == bAncestor) {
+					answer = aAncestor;
+				} else {
+					break;
+				}
+			}
+			bw.write(answer+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
 
-        T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            N = Integer.parseInt(br.readLine());
-            parents = new int[N + 1];
-            answer = 0;
 
-            // 부모 관계를 설정
-            for (int j = 0; j < N - 1; j++) {
-                st = new StringTokenizer(br.readLine());
-                int parentNode = Integer.parseInt(st.nextToken());
-                int childNode = Integer.parseInt(st.nextToken());
-                parents[childNode] = parentNode;
-            }
-
-            st = new StringTokenizer(br.readLine());
-            int nodeA = Integer.parseInt(st.nextToken());
-            int nodeB = Integer.parseInt(st.nextToken());
-
-            // A의 조상 리스트 생성
-            list = new ArrayList<>();
-            list.add(nodeA);
-            findRoutes(nodeA);
-            List<Integer> parentsA = list;
-
-            // B의 조상 리스트 생성
-            list = new ArrayList<>();
-            list.add(nodeB);
-            findRoutes(nodeB);
-            List<Integer> parentsB = list;
-
-            // 조상 리스트를 비교하여 가장 깊은 공통 조상을 찾는다
-            answer = 0;  // 기본값을 0으로 설정 (만약 공통 조상이 없다면)
-            for (int j = 0; j < Math.min(parentsA.size(), parentsB.size()); j++) {
-                int aAncestor = parentsA.get(parentsA.size() - 1 - j);
-                int bAncestor = parentsB.get(parentsB.size() - 1 - j);
-                if (aAncestor == bAncestor) {
-                    answer = aAncestor;  // 공통 조상을 찾으면 갱신
-                } else {
-                    break;  // 더 이상 공통 조상이 없으므로 종료
-                }
-            }
-
-            bw.write(answer + "\n");
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    private static void findRoutes(int node) {
-        if (parents[node] != 0) {  // 루트가 아닌 노드는 부모가 0이 아니므로
-            list.add(parents[node]);
-            findRoutes(parents[node]);
-        }
-    }
+	private static void findRoutes(int node) {
+		if(parents[node] != 0) {
+			list.add(parents[node]);
+			findRoutes(parents[node]);
+		} 
+	}
 }
