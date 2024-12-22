5e238ddc93355821cfe7b04ff3b09b00a60db18b
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-12-22 21:16:19 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 424 ms, Memory: 34492 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,45 +1,57 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
- 
-public class Main {
-	
-	public static int N, M;
-	public static int[] arr;
-	public static int[] selected;
-	public static int[] answer;
-	public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	N = Integer.parseInt(st.nextToken());
-    	arr = new int[N];
-    	selected = new int[1000001];
-    	answer = new int[N + 1];
-    	st = new StringTokenizer(br.readLine());
-    	for(int i=0;i<N;i++) {
-    		arr[i] = Integer.parseInt(st.nextToken());
-    		selected[ arr[i] ] = i + 1;
-    		
-    	}
-    	
-    	for(int i=0;i<N;i++) {
-    		int start = arr[i];
-    		for(int j= start * 2; j< 1000001; j+=start) {
- 
-    			if(selected[j] > 0) {
-    				answer[ selected[j] ] -= 1;
-    				answer[ selected[start] ] += 1;
-    			}
-    		}
-    	}
-    	
-    	for(int i=1;i<=N;i++) {
-    		System.out.print(answer[i]+" ");
-    	}
-    	
+
+
+class Main {
+	// 에라토스테네스의 체 : 배수를 찾아서 모두 지우면 소수만 남길 수 있다
+	static int N;
+	static int[] score,player;
+	static int[] cardInHand;
+	static int max = 0;
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(br.readLine());
+		score = new int[N+1];
+		player = new int[N+1];
+		cardInHand = new int[1000001];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=N;i++) {
+			int card = Integer.parseInt(st.nextToken());
+			cardInHand[card] = i;
+			player[i] = card;
+			max = Math.max(max, card);
+		}
+
+		for(int i=1;i<=N;i++) {
+			int start = player[i];
+			for(int j=start*2;j<=max;j+=start) {
+//				System.out.println(start+" "+j+" "+cardInHand[j]);
+				if(cardInHand[j]>0) {
+					score[i]++;
+					score[cardInHand[j]]--;
+				}
+			}
+		}
+
+		for(int i=1;i<=N;i++) {
+			bw.write(score[i]+" ");
+		}
+		
+
+		bw.flush();
+		bw.close();
 	}
-	
-	
 }
