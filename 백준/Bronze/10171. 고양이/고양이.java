ff493d23c051a22d2b2584c83c35935f785fc6fb
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2024-12-29 19:42:41 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+	static int N,X;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("\\    /\\\n");
+		bw.write(" )  ( ')\n");
+		bw.write("(  /  )\n");
+		bw.write(" \\(__)|\n");
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+
+}
