59a1231ec4f5e56a2c376930ad1eb413c0623b52
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-01-07 13:11:27 +0900

    [Gold IV] Title: LCS 2, Time: 140 ms, Memory: 18900 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -14,7 +14,6 @@ public class Main {
 		
 		fir = br.readLine();
 		sec = br.readLine();
-		String str="";
 		if(fir.length()<sec.length()) {
 			String tmp = sec;
 			sec = fir;
@@ -25,23 +24,24 @@ public class Main {
 		for(int i=1;i<=fir.length();i++) {
 			for(int j=1;j<=sec.length();j++) {
 				if(fir.charAt(i-1)==sec.charAt(j-1)) {
+					// dp[i][j] =  dp[i][j-1]+1; -> 똑같은 글자가 두개 이상 있으면 j-1로 받을 때 문제 발생
 					dp[i][j] =  dp[i-1][j-1]+1;
 				} else {
 					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 				}
 			}
 		}
+		
 		int x = fir.length();
 		int y = sec.length();
 
 		while (x != 0 && y != 0) {
-//			System.out.println(x+" "+y+" "+dp[x][y]);
 			if (dp[x - 1][y] == dp[x][y]) { // 왼쪽값과 같다
 				x -= 1;
 			} else if (dp[x][y - 1] == dp[x][y]) { // 윗쪽값과 같다.
 				y -= 1;
 			} else { // 왼쪽값과 윗쪽값과 같은 경우가 없다.
-				str += fir.charAt(x-1);
+				sb.append(fir.charAt(x-1));
 				x -= 1;
 				y -= 1;
 			}
@@ -50,9 +50,7 @@ public class Main {
 
 		System.out.println(dp[fir.length()][sec.length()]);
 		if(dp[fir.length()][sec.length()]!=0) {
-			for (int i = str.length()-1; i >= 0 ; i--) {
-				System.out.print(str.charAt(i));
-			}
+			System.out.println(sb.reverse().toString());
 		}
 		
 	}
