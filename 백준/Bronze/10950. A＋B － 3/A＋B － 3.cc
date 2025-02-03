e4c22a2f92d22184df7ea03cabfa2c0cc1c0bc13
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-02-03 15:13:26 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+int main(void)
+{
+
+	int N;
+	cin >> N;
+	for (int i = 0;i < N;i++)
+	{
+		int A;
+		int B;
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+}
+
