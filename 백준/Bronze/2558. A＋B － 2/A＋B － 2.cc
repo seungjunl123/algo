d6d922d9aff8520aee25ab0627185c33d5425444
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-02-27 23:50:13 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#define ll long long
+using namespace std;
+
+int S, P;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> S >> P;
+
+	cout << S + P << endl;
+}
