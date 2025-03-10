3b7337959cf33080b5b33a3da490f1f0358421be
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-03-10 14:01:09 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -7,16 +7,16 @@ int main()
 {
 	int N, M;
 	cin >> N >> M;
-	vector<pair<int, int>> app(N);
+	vector<pair<int, int>> arr(N);
 	vector<int> sum(N);
 
 	for (int i = 0; i < N; i++) {
-		cin >> app[i].first;
+		cin >> arr[i].first;
 	}
 
 	for (int i = 0; i < N; i++) {
-		cin >> app[i].second;
-		sum[i] = app[i].second;
+		cin >> arr[i].second;
+		sum[i] = arr[i].second;
 		if (i > 0)
 			sum[i] += sum[i - 1];
 	}
@@ -30,34 +30,32 @@ int main()
 
 	vector<vector<int>> dp(N, vector<int>(sum[N - 1] + 1, 0));
 
-	for (int c = app[0].second; c <= sum[N - 1]; c++)
-		dp[0][c] = app[0].first;
+	for (int c = arr[0].second; c <= sum[N - 1]; c++)
+		dp[0][c] = arr[0].first;
 
-	int min_cost = -1;
-	int test_cost = -1; // 테스트 용
+	int answer = 0;
 
 	for (int n = 1; n < N; n++) {
 		for (int c = 0; c <= sum[N - 1]; c++) {
-			if (c < app[n].second) // c가 app.second보다 작으면 n번째 앱은 못 끈다.
+			if (c < arr[n].second) // c가 app.second보다 작으면 n번째 앱은 못 끈다.
 			{
 				dp[n][c] = dp[n - 1][c];
 			}
 			else
 			{
-				dp[n][c] = max(dp[n - 1][c - app[n].second] + app[n].first, dp[n - 1][c]);
+				dp[n][c] = max(dp[n - 1][c - arr[n].second] + arr[n].first, dp[n - 1][c]);
 			}
 
 		}
 	}
 
-	// 정답 출력문
 	for (int c = 0; c <= sum[N - 1]; c++) {
 		if (dp[N - 1][c] >= M)
 		{
-			min_cost = c;
+			answer = c;
 			break;
 		}
 	}
 
-	cout << min_cost; // 이 부분을 test_cost로 바꿀 시 98%에서 틀림.
+	cout << answer;
 }
