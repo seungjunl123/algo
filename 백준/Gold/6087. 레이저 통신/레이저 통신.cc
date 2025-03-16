01281a688d7ab5d29d3b64fa8d4e761b44f7f54c
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-03-16 17:19:55 +0900

    [Gold III] Title: 레이저 통신, Time: 0 ms, Memory: 2564 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.cc
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -5,92 +5,118 @@
 #define INF 987654321
 using namespace std;
 
-struct Node {
-    int r, c, dir, count;
-    Node(int R, int C, int Dir, int Count) : r(R), c(C), dir(Dir), count(Count) {}
-};
+// BFS로 앞, 좌, 우 이동하면서 이동
 
+struct Node
+{
+	int r;
+	int c;
+	int dir;
+	int count;
+	Node(int R, int C, int Dir, int Count)
+		: r(R), c(C), dir(Dir), count(Count)
+	{}
+};
 int W, H;
 int answer = INF;
 vector<vector<int>> map;
 vector<pair<int, int>> LocationC;
-vector<vector<vector<int>>> checked; // 방향별로 체크
+vector<vector<vector<int>>> checked;
 
-int dr[] = {1, 0, -1, 0};  // 하, 좌, 상, 우 (0, 1, 2, 3)
-int dc[] = {0, -1, 0, 1};
+int dr[] = { 1,0,-1,0 };
+int dc[] = { 0,-1,0,1 };
 
-void BFS() {
-    queue<Node> q;
-    int startR = LocationC[0].first;
-    int startC = LocationC[0].second;
+void BFS()
+{
+	queue<Node> q;
+	int startR = LocationC[0].first;
+	int startC = LocationC[0].second;
 
-    for (int i = 0; i < 4; i++) {
-        int nr = startR + dr[i];
-        int nc = startC + dc[i];
+	for (int i = 0;i < 4;i++)
+	{
+		int nr = startR + dr[i];
+		int nc = startC + dc[i];
 
-        if (nr >= 0 && nc >= 0 && nr < H && nc < W && map[nr][nc] != 1) {
-            q.push(Node(nr, nc, i, 0));
-            checked[nr][nc][i] = 0;
-        }
-    }
+		if (nr >= 0 && nc >= 0 && nr < H && nc < W && map[nr][nc] != 1)
+		{
+			q.push(Node(nr, nc, i,0));
+			checked[nr][nc][i] = 0;
+		}
+	}
 
-    while (!q.empty()) {
-        int curR = q.front().r;
-        int curC = q.front().c;
-        int curDir = q.front().dir;
-        int curCount = q.front().count;
-        q.pop();
+	while (!q.empty())
+	{
+		int curR = q.front().r;
+		int curC = q.front().c;
+		int curDir = q.front().dir;
+		int curCount = q.front().count;
 
-        if (curR == LocationC[1].first && curC == LocationC[1].second) {
-            answer = min(answer, curCount);
-            continue;
-        }
 
-        for (int i = 0; i < 3; i++) {
-            int nextDir;
-            switch (i) {
-                case 0:
-                    nextDir = curDir;
-                    break;
-                case 1:
-                    nextDir = (curDir + 1) % 4; // 시계 방향 회전
-                    break;
-                case 2:
-                    nextDir = (curDir + 3) % 4; // 반시계 방향 회전
-                    break;
-            }
+		q.pop();
 
-            int nr = curR + dr[nextDir];
-            int nc = curC + dc[nextDir];
-            int nCount = (i == 0) ? curCount : curCount + 1;
+		//cout << "cur R : " << curR << " cur C : "<< curC << " cur Count : " << curCount << " cur Dir : " << curDir << " 현재 체크: "<<checked[curR][curC] << endl;
+		if (curR == LocationC[1].first && curC == LocationC[1].second)
+		{
+			if (curCount < answer) answer = curCount;
+			continue;
+		}
 
-            if (nr < 0 || nc < 0 || nr >= H || nc >= W || map[nr][nc] == 1) continue;
-            if (checked[nr][nc][nextDir] <= nCount) continue;
+		for (int i = 0;i < 3;i++)
+		{
+			int nextDir;
+			switch (i)
+			{
+			case 0 : 
+				nextDir = curDir;
+				break;
+			case 1:
+				nextDir = (curDir + 1) % 4;
+				break;
+			case 2:
+				nextDir = (curDir + 3) % 4;
+				break;
+			default:
+				break;
+			}
 
-            checked[nr][nc][nextDir] = nCount;
-            q.push(Node(nr, nc, nextDir, nCount));
-        }
-    }
+			int nr = curR + dr[nextDir];
+			int nc = curC + dc[nextDir];
+			int nCount = i == 0 ? curCount : curCount + 1;
+
+			if (nr < 0 || nc < 0 || nr >= H || nc >= W || map[nr][nc] == 1) continue;
+			if ( checked[nr][nc][nextDir] <= nCount) continue;
+
+
+			checked[nr][nc][nextDir] = nCount;
+
+			q.push(Node(nr, nc, nextDir, nCount));
+		}
+	}
 }
 
 int main() {
-    cin >> W >> H;
-    map.resize(H, vector<int>(W, 0));
-    checked.resize(H, vector<vector<int>>(W, vector<int>(4, INF))); // 3차원 배열 초기화
+	cin >> W >> H;
+	map.resize(H, vector<int>(W, 0));
+	checked.resize(H, vector<vector<int>>(W, vector<int>(4, INF)));
 
-    for (int i = 0; i < H; i++) {
-        string str;
-        cin >> str;
-        for (int j = 0; j < W; j++) {
-            if (str[j] == '*') {
-                map[i][j] = 1;
-            } else if (str[j] == 'C') {
-                LocationC.push_back({i, j});
-            }
-        }
-    }
+	for (int i = 0;i < H;i++)
+	{
+		string str;
+		cin >> str;
+		for (int j = 0;j < W;j++)
+		{
+			if (str[j] == '*')
+			{
+				map[i][j] = 1;
+			}
+			else if (str[j] == 'C')
+			{
+				LocationC.push_back(make_pair(i, j));
+			}
+		}
+	}
 
-    BFS();
+	BFS();
 
-    cout << answer << endl;
+	cout << answer << endl;
 }
