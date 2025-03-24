1afe6f9c74a0cd3dc08d9a27956917f42eeaa0b4
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-03-24 18:32:02 +0900

    [Gold II] Title: 피보나치 수 3, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+long long N;
+map<long long, long long> list;
+
+long long DivideAndConquer(long long N)
+{
+
+	if (list.find(N) != list.end())
+		return list[N];
+
+
+	long long value;
+	if (N % 2 == 0)
+	{
+		value = DivideAndConquer(N / 2l + 2) * DivideAndConquer(N / 2) +
+			DivideAndConquer(N / 2l + 1) * DivideAndConquer(N / 2l - 1);
+	}
+	else
+	{
+		value = DivideAndConquer(N / 2l + 3) * DivideAndConquer(N / 2l) +
+			DivideAndConquer(N / 2l + 2) * DivideAndConquer(N / 2l - 1);
+	}
+	value %= 1000000l;
+	list.emplace(N, value);
+	return value;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	cin >> N;
+	list.emplace(1, 0);
+	list.emplace(2, 1);
+	list.emplace(3, 1);
+	list.emplace(4, 2);
+	list.emplace(5, 3);
+
+	long long answer = DivideAndConquer(N + 1);
+
+	cout << answer;
+}
+
