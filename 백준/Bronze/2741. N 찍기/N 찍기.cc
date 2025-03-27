246e066539ec5210f6e2b60490a3a381312ae253
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-03-27 23:23:20 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+
+// 가장 가까운 상어는?
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1;i <= N;i++)
+	{
+		cout << i << "\n";
+	}
+
+	cout << endl;
+}
+
