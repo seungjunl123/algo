4ce68326b17de13df174052160b676812e37def7
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-04-21 12:52:37 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.21 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,31 +1,26 @@
 #include <string>
 #include <vector>
 #include <queue>
-
+#include <algorithm>
 using namespace std;
 
-long long getGCD(long long a, long long b) {
-    while (b != 0) {
-        long long temp = a % b;
-        a = b;
-        b = temp;
-    }
-    return a;
-}
-
-int solution(vector<int> arr) {
+long long solution(vector<int> arr) {
     queue<long long> q;
-    for(int i = 0; i < arr.size(); i++) {
-        q.push(arr[i]); 
+    for (int i = 0; i < arr.size(); i++) {
+        q.push(arr[i]);
     }
 
-    while(q.size() > 1) {
+    while (q.size() > 1) {
         long long first = q.front(); q.pop();
         long long second = q.front(); q.pop();
 
-        long long GCD = getGCD(first, second);
-        q.push(first * second / GCD);  // LCM
+        long long GCD = 0;
+        for (int i = 1; i <= min(first,second); i++) {
+            if (first % i == 0 && second % i == 0) GCD = i;
+        }
+        q.push(first * second / GCD);
     }
 
-    return q.front();
+    return q.front();  // long long으로!
 }
+
