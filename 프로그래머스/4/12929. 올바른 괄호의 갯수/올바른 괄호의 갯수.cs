10bd1b7eb384d0fcf255681bf90a87189ed50754
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-05-01 13:32:52 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cs
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cs
@@ -1,23 +1,15 @@
-public class Solution {
-    int answer = 0;
-    
-    public void permutation(int openCnt, int closeCnt, int sum)
-    {
-        if(sum < 0 || openCnt < 0 || closeCnt<0 ) return;
-        if(openCnt == 0 && closeCnt == 0 && sum == 0) answer++;
-         
-        permutation(openCnt-1, closeCnt, sum+1);
-        permutation(openCnt, closeCnt-1, sum-1);
-        return;
-    }
-    
+class Solution {
     public int solution(int n) {
-        
-        
-        // 재귀
-        permutation(n, n, 0);
-        
-        
-        return answer;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+ 
+        for (int i = 2; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                dp[i] += dp[i - j - 1] * dp[j];
+            }
+        }
+ 
+        return dp[n];
     }
 }
