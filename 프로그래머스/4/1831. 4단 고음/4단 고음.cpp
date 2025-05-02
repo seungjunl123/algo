a500a2a819bf6e8bc802c009f7331059b9a4ca42
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-05-02 18:55:45 +0900

    [level 4] Title: 4단 고음, Time: 141.52 ms, Memory: 4.29 MB -BaekjoonHub

--- 프로그래머스/4/1831. 4단 고음/4단 고음.cpp
+++ 프로그래머스/4/1831. 4단 고음/4단 고음.cpp
@@ -1,38 +1,36 @@
-int answer;
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+    int answer = 0;
 
-int cntThree(int k){
+int cntThree(int k) {
     int num = k;
     int ret = 0;
-    while(num/3 != 0){
-        num/=3;
+    while (num / 3 != 0) {
+        num /= 3;
         ret++;
     }
 
     return ret;
 }
 
-void dfs(int n, int cntMultiple, int cntPlus){
 
-    if(n==1){
-        answer++;
+void GoTo_1(int n, int MulCount, int AddCount)
+{
+
+    if(n == 1)
+    {
+        answer += 1;
         return;
     }
-
-    if(n%3==0){
-        if ((cntMultiple+1) *2 <=  cntPlus){
-            dfs(n/3, cntMultiple+1, cntPlus);
-        }
+    
+    if(n%3==0 && (MulCount+1)*2 <=AddCount ) {
+        GoTo_1(n/3, MulCount+1, AddCount);
     }
-
-    if( (cntMultiple+cntThree(n))*2 >= cntPlus+1){
-        dfs(n-1, cntMultiple, cntPlus+1);
-    }
-
-    return;
+    if((MulCount+cntThree(n))*2 >=AddCount+1)
+        GoTo_1(n-1, MulCount, AddCount+1);
 }
 
 int solution(int n) {
-    answer =0;
-    dfs(n, 0, 0);
+    answer = 0;
+    GoTo_1(n, 0, 0);
     return answer;
 }
