76730fecef63d2b5a52675cab6c97472f6281144
seungjunl123 <156068215+seungjunl123@users.noreply.github.com>
2025-05-08 14:08:45 +0900

    [level 3] Title: 거스름돈, Time: 6.28 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std; 
+
+int solution(int n, vector<int> money) {
+    int answer = 0;
+    
+	vector<int> dp(n+1);
+		
+	dp[0] = 1;
+
+		
+	for(int i=0;i<money.size();i++) {
+		for(int j=1;j<=n;j++) {
+			if(j>=money[i])	dp[j] += dp[j-money[i]];
+		}
+	}
+    
+    return dp[n];
+}
